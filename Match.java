bf265abf2f425bacba0014bb198660a6c0cbc65a
Utkurbek Safaev <safaevub@gmail.com>
2023-06-18 13:42:49 +0200

    Match class

---
+++ Match.java
@@ -0,0 +1,28 @@
+package radar;
+
+public class Match {
+    private String homeTeam;
+    private String awayTeam;
+    private int homeScore;
+    private int awayScore;
+
+    public Match(String homeTeam, String awayTeam) {
+        this.homeTeam = homeTeam;
+        this.awayTeam = awayTeam;
+        this.homeScore = 0;
+        this.awayScore = 0;
+    }
+
+    public void updateScore(int homeScore, int awayScore) {
+        this.homeScore = homeScore;
+        this.awayScore = awayScore;
+    }
+
+    public int getTotalScore() {
+        return homeScore + awayScore;
+    }
+
+    public String getSummary() {
+        return homeTeam + " " + homeScore + " - " + awayTeam + " " + awayScore;
+    }
+}
